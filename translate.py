aebcbba918f2d0b5e6c9b2621b8075cc6d1924d4
ikofan <bj1061@163.com>
2019-07-29 16:45:24 +0800

    Update translate.py
    finish translate coding

--- translate.py
+++ translate.py
@@ -1,7 +1,7 @@
 import openpyxl
 
 
-wb = openpyxl.load_workbook("D:\\360che\\dict.xlsx")
+wb = openpyxl.load_workbook("D:\\360che\\test\\dict.xlsx")
 ws = wb.active
 
 cn = []
@@ -18,4 +18,17 @@ for cell in ws["B"]:
 dict_list = dict(zip(cn, en))
 # 一开始还不会把列表转为字典，上面这个方法简单啊，学习了。
 
-print(dict_list)
+wb = openpyxl.load_workbook("D:\\360che\\test\\results_new_sorted.xlsx")
+ws1 = wb["Sheet1"]
+ws2 = wb.copy_worksheet(ws1)
+ws2.title = 'EN'
+
+# print(dict_list[t])
+
+for col in ws2.columns:
+    for cell in col:
+        if str(cell.value) in dict_list:
+            cell.value = dict_list[cell.value]
+
+
+wb.save("D:\\360che\\test\\translate.xlsx")
